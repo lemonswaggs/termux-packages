63e6ab510db33679f9161f186f33c2461570fab0
its-pointless <bored@goat.si>
2016-04-18 09:47:44 +1000

    was using autoreconf and that for some reason used old version of ltmain.sh

--- packages/aria2/build.sh
+++ packages/aria2/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://aria2.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Multi-protocol & multi-source command-line download utility supporting HTTP/HTTPS, FTP, BitTorrent and Metalink"
-TERMUX_PKG_VERSION=1.20.0
+TERMUX_PKG_VERSION=1.22.0
 TERMUX_PKG_SRCURL=https://github.com/tatsuhiro-t/aria2/releases/download/release-$TERMUX_PKG_VERSION/aria2-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="c-ares, openssl, libxml2, libgnustl"
 # sqlite3 is only used for loading cookies from firefox or chrome:
@@ -9,5 +9,4 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-openssl --without-gnutls --without-libuv
 termux_step_pre_configure () {
 	export CXXFLAGS="$CXXFLAGS -lgnustl_shared"
 	cd $TERMUX_PKG_SRCDIR
-	autoreconf
 }
