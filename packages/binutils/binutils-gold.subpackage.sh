150377d0a7823260617a12fce932f857ec77e9ce
its-pointless <bored@goat.si>
2017-03-26 11:05:42 +1100

    enable binutils-gold

---
+++ packages/binutils/binutils-gold.subpackage.sh
@@ -0,0 +1,3 @@
+TERMUX_SUBPKG_INCLUDE="bin/ld.gold"
+TERMUX_SUBPKG_DEPENDS="binutils"
+TERMUX_SUBPKG_DESCRIPTION="plugin supoort for lto etc"
--- packages/binutils/build.sh
+++ packages/binutils/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Collection of binary tools, the main ones being ld, the 
 TERMUX_PKG_VERSION=2.28
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/binutils/binutils-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=cd717966fc761d840d451dbd58d44e1e5b92949d2073d75b73fccb476d772fcf
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-werror --with-system-zlib"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-gold --enable-plugins --disable-werror --with-system-zlib"
 TERMUX_PKG_EXTRA_MAKE_ARGS="tooldir=$TERMUX_PREFIX"
 TERMUX_PKG_RM_AFTER_INSTALL="share/man/man1/windmc.1 share/man/man1/windres.1 bin/ld.bfd"
 
