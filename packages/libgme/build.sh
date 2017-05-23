3f55a52c5b0a5dcfe040107c362330294fd47cab
its-pointless <bored@goat.si>
2017-05-23 15:19:37 +1000

    to play game music like chiptunes etc

---
+++ packages/libgme/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=https://bitbucket.org/mpyne/game-music-emu
+TERMUX_PKG_DESCRIPTION="emulated game music"
+TERMUX_PKG_VERSION=0.6.1
+TERMUX_PKG_SRCURL=https://bitbucket.org/mpyne/game-music-emu/downloads/game-music-emu-0.6.1.tar.bz2
+TERMUX_PKG_SHA256=dc11bea098072d540d4d52dfb252e76fc3d3af67ee2807da48fbd8dbda3fd321
